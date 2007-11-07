c8efca017ef980dd2f08f8b71d98e7d71f010f47
Eivind Uggedal <eu@redflavor.com>
2007-11-08 00:11:41 +0100

    Added support for viewing single entries.

--- reprise.rb
+++ reprise.rb
@@ -6,22 +6,40 @@ get '/' do
   haml :index
 end
 
-# Returns all textual entries with file names.
-def entries
-  Dir[File.dirname(__FILE__) + '/entries/*'].reverse.collect do |file|
-    { :body => File.read(file) }.merge(meta_from_filename(file))
+get '/:slug' do
+  @entry = nil
+  entries.each do |entry|
+    if entry[:slug] == params[:slug]
+      @entry = entry
+      break
+    end
   end
+  haml :entry
 end
 
-# Parses an entry's filename into date, slug, and title.
-def meta_from_filename(file)
-  filename = File.basename(file)
-  results = filename.scan(/(\d\d\d\d\.\d\d.\d\d)\.(.+)/).first
-  date = Date.parse(results[0])
-  slug = results[1].gsub(/\./, '-')
-  title = results[1].gsub(/\./, ' ')
+private
 
-  { :filename => filename,
-    :title => title,
-    :date => date }
-end
+  # Returns all textual entries with file names.
+  def entries
+    Dir[File.dirname(__FILE__) + '/entries/*'].reverse.collect do |file|
+      { :body => File.read(file) }.merge(meta_from_filename(file))
+    end
+  end
+
+  # Parses an entry's filename into date, slug, and title.
+  def meta_from_filename(file)
+    filename = File.basename(file)
+    results = filename.scan(/([\d]{4}.\d\d.\d\d)\.(.+)/).first
+    date = Date.parse(results[0])
+    title = results[1].gsub(/\./, ' ')
+
+    { :filename => filename,
+      :date => date, 
+      :title => title,
+      :slug => slugify(title) }
+  end
+
+  # Removes non-alphanumeric characters and substitutes spaces for hyphens.
+  def slugify(string)
+    string.gsub(/[^\w\s-]/, '').gsub(/\s+/, '-').downcase
+  end
