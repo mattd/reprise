d5434349e6c0ca2db9aca31706e25edc187bcf72
Eivind Uggedal <eu@redflavor.com>
2008-06-29 23:32:59 +0200

    Seperated out readme and license.

--- reprise.rb
+++ reprise.rb
@@ -1,32 +1,4 @@
 #!/usr/bin/env ruby
-# Copyright (c) 2007-2008 Eivind Uggedal <eu@redflavor.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to
-# deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-# sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-#
-#
-# Reprise - As minimal a hAtom blog as possible
-#
-#   1. vi entries/YYYY.MM.DD.Entry.Title.in.Camel.Case
-#   2. gem install BlueCloth rubypants haml
-#   3. ./reprise.rb
-#   4. Hook up public/ to a web server like nginx
-
 %w(rubygems bluecloth rubypants haml sass stringio time).each { |lib| require lib }
 
 TITLE = 'Research Journal'
